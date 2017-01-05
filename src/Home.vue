9eaf7a1dba2be6817a9b17de3baf2e710f7ee664
antcosic <antcosic@foi.hr>
2017-01-05 21:57:31 +0100

    Start store with items(posts)

--- src/Home.vue
+++ src/Home.vue
@@ -1,5 +1,52 @@
 <template>
-
-        <h2>Home</h2>
-
+        <div>
+                <form id="search">
+                        Search <input name="query" v-model="searchQuery">
+                </form>
+                <demo-grid
+                        :data="gridData"
+                        :columns="gridColumns"
+                        :filter-key="searchQuery">
+                </demo-grid>
+        </div>
 </template>
+
+<style>
+
+</style>
+<script>
+        import GridComponent from './GridComponent.vue';
+        import axios from 'axios';
+        import Vue from 'vue';
+        export default{
+                data() {
+                        return {
+                                searchQuery: '',
+                                gridColumns: ['userId','id', 'title', 'body'],
+                                gridData: []
+                        }
+                },
+                created(){
+                        this.fetchData();
+                },
+                methods:{
+
+                        fetchData(){
+                                var self = this;
+                                axios.get('https://jsonplaceholder.typicode.com/posts')
+                                        .then(function(response) {
+                                                Vue.set(self.$data, 'gridData',response.data)
+                                                console.log(JSON.stringify(response.data));
+                                        })
+                                        .catch(function(error) {
+                                                console.log(error);
+                                        });
+                         },
+
+                },
+
+                components:{
+                        'demo-grid': GridComponent
+                }
+        }
+</script>
