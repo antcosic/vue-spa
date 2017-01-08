1df1ccbf2df4431203f051a98252c53a8417b616
antcosic <antcosic@foi.hr>
2017-01-08 14:06:36 +0100

    Area and line chart

--- src/Graphs.vue
+++ src/Graphs.vue
@@ -1,72 +1,53 @@
 <template>
-    <div>
+    <div style="text-align: center;">
+        <h2> Graphs </h2>
+        <div class="menuRow">
+            <div class="menuItems">
+                <area-chart></area-chart>
+            </div>
+            <div class="menuItems">
+                <line-chart></line-chart>
+            </div>
 
-        <div id="chart_div" style="width: 100%; height: 500px;"></div>
+        </div>
 
-        <h1>This is graph</h1>
     </div>
 </template>
 
 <style scoped>
-
+    .menuItems {
+        -webkit-flex: 1;
+        -ms-flex: 1;
+        flex: 1;
+    }
+    .menuRow {
+        padding-left: 50px;
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        position: relative;
+    }
 </style>
 <script>
-    import * as d3 from 'd3';
-    import * as nvd3 from 'nvd3';
+    import Graph2 from './Graph2.vue';
+    import Graph1 from './Graph1.vue';
 
-
-    google.charts.load('current', {'packages':['corechart']});
-      google.charts.setOnLoadCallback(drawChart);
-
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses'],
-          ['2013',  1000,      400],
-          ['2014',  1170,      460],
-          ['2015',  660,       1120],
-          ['2016',  1030,      540]
-        ]);
-
-        var options = {
-          title: 'Company Performance',
-          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
-          vAxis: {minValue: 0}
-        };
-
-        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
-        chart.draw(data, options);
-      }
 
     export default {
         data(){
             return{
 
+            }
 
-           }
         },
 
         created(){
-            this.ispisD();
-            this.ispisNvd();
 
         },
-        methods: {
-            sine() {
-                var sin = [];
-                for (var i = 0; i < 100; i++) {
-                    sin.push({x: i, y: Math.sin(i/10)});
-                }
-                return sin;
-            },
-            ispisD(){
-                console.log(d3);
-
-            },
-            ispisNvd(){
-                console.log(nvd3);
-
-            }
-
+        components: {
+            'area-chart': Graph2,
+            'line-chart': Graph1
         }
 
 
